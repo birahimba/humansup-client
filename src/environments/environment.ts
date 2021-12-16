04fbc23851eac3eb043d3b69a86404719e718142
Elhadj Birahim Ba <Birahimba1@hotmail.com>
2021-12-16 19:25:14 +0100

    test 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
+export const environment = {
+  production: false,
   apiKey: "AIzaSyBdmZ4sNLuayCiwxQuou4ffrI1mDfJ9Cgk",
   authDomain: "humansup-client.firebaseapp.com",
   databaseURL: "https://humansup-client-default-rtdb.europe-west1.firebasedatabase.app",
@@ -15,3 +13,12 @@ export const firebaseConfig = {
   appId: "1:680723754915:web:d3eff69ba15b790153b416",
   measurementId: "G-YT34C6SML3"
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
