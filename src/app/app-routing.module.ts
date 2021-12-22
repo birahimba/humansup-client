85de2a5282082aedf6c41c929287f81602b6744a
Elhadj Birahim Ba <Birahimba1@hotmail.com>
2021-12-22 15:22:47 +0100

    add page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EventsComponent } from './events/events.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ProgramsComponent } from './programs/programs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'programs', component: ProgramsComponent },
+  { path: 'events', component: EventsComponent},
+  { path: 'news', component: NewsComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', pathMatch: 'full', component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { EventsComponent } from './events/events.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { FooterComponent } from './footer/footer.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ProgramsComponent,
+    EventsComponent,
+    NewsComponent,
+    ContactComponent,
+    PagenotfoundComponent,
+    FooterComponent,
+    MaintenanceComponent
   ],
   imports: [
     BrowserModule,
