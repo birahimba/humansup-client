9b753ee5412c6f641fbc1b9c8ddb4a6c214638f4
Elhadj Birahim Ba <Birahimba1@hotmail.com>
2022-01-22 16:10:26 +0100

    update

--- src/app/client-component/home/home.component.ts
+++ src/app/client-component/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private meta: Meta,
+    private titleService: Title
+  ) { }
+
 
   ngOnInit(): void {
+
+    this.titleService.setTitle('Humansup: School Of Humanitarian Sciences');
+    this.meta.addTag({
+      name: 'Humansup',
+      content: 'Humansup'
+    });
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'The school intends to train the next generation of humanitarians in the Global South with a view to changing the operational landscape in humanitarian contexts.' 
+          
+      });
+
   }
 
 }
