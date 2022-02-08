c0004dd7d87b5f80d39e114c459439d00609c359
Elhadj Birahim Ba <Birahimba1@hotmail.com>
2022-02-08 19:38:00 +0000

    favicon update

--- src/app/client-component/navbar/navbar.component.ts
+++ src/app/client-component/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LanguageService } from 'src/app/services/language/language.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private languageService : LanguageService) { }
+
 
   ngOnInit(): void {
+    
   }
 
   ToggleNavBar () {
